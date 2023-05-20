b33914d18098394bce99f1ae78e8f34ab320273b
sulincix <parduscix@yandex.ru>
2023-05-20 16:20:52 +0000

    fix firmware installation

--- mkiso.sh
+++ mkiso.sh
@@ -37,7 +37,7 @@ if [[ "FIRMWARE" != "" ]] ; then
     cd rootfs/tmp
     tar -xvf linux-firmware.tar.gz
     cd linux-firmware-$version
-    ./copy-firmware.sh ../../../lib/firmware
+    ./copy-firmware.sh ../../lib/firmware
     cd ../../..
     rm -rf rootfs/tmp/linux-firmware*
 fi
