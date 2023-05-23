300d88183671aa23d4a3c55b5bb1d4a3ae8845e8
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 (𐰽𐰆𐰞𐰃𐰤𐰆𐰽) <parduscix@yandex.ru>
2023-05-23 16:41:55 +0000

    Update mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -7,24 +7,43 @@ fi
 umask 022
 mkdir isowork/live -p
 mkdir isowork/boot/grub -p
+if [[ "$REPO" == "" ]] ; then
+    export REPO='https://gitlab.com/turkman/packages/binary-repo/-/raw/master/$uri'
+fi
 if ! which ympstrap >/dev/null ; then
     wget https://gitlab.com/turkman/devel/sources/ymp/-/raw/master/scripts/ympstrap -O /bin/ympstrap
     chmod +x /bin/ympstrap
 fi
-ympstrap rootfs live-boot linux openrc
+# create rootfs
+ympstrap rootfs live-boot linux openrc gnupg
+# add gpg key
+wget -O rootfs/tmp/ymp-index.yaml.asc ${REPO/\$uri/ymp-index.asc}
+chroot rootfs gpg --import /tmp/ymp-index.yaml.asc
+rm -f rootfs/tmp/ymp-index.yaml.asc
+# openrc settings
 ln -s openrc-init rootfs/sbin/init || true
 ln -s agetty rootfs/etc/init.d/agetty.tty1 || true
 chroot rootfs rc-update add agetty.tty1
+# system configuration
 echo -e "31\n31\n" | chroot rootfs passwd
 echo "nameserver 1.1.1.1" > rootfs/etc/resolv.conf
+# bind mount
 for dir in dev sys proc run tmp ; do
     mount --bind /$dir rootfs/$dir
 done
+# customize
 if [[ -f custom ]] ; then
     cp custom rootfs/tmp/custom
     chroot rootfs bash -ex /tmp/custom
     rm rootfs/tmp/custom
+elif [[ -d custom ]] ; then
+    for file in $(ls custom) ; do
+        cp custom/$file rootfs/tmp/custom
+        chroot rootfs bash -ex /tmp/custom
+        rm rootfs/tmp/custom
+    done
 fi
+# bind unmount
 for dir in dev sys proc run tmp ; do
     while umount -lf -R rootfs/$dir ; do : ; done
 done
@@ -41,9 +60,12 @@ if [[ "FIRMWARE" != "" ]] ; then
     cd ../../..
     rm -rf rootfs/tmp/linux-firmware*
 fi
+# create squashfs
 mksquashfs rootfs isowork/live/filesystem.squashfs -comp gzip -wildcards
+# copy kernel and initramfs
 install rootfs/boot/vmlinuz-* isowork/linux
 install rootfs/boot/initrd.img-* isowork/initrd.img
+# create grub config
 cat > isowork/boot/grub/grub.cfg <<EOF
 insmod all_video
 menuentry TurkMan {
@@ -51,4 +73,5 @@ menuentry TurkMan {
     initrd /initrd.img
 }
 EOF
+# create iso image
 grub-mkrescue -o turkman.iso isowork
