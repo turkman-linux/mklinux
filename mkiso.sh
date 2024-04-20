e60b01becc874226406bdc9343dff3c55bb757a3
𐰀𐰞𐰃:𐰺𐰃𐰔𐰀:𐰚𐰀𐰾𐰚𐰃𐰤 <aliriza.keskin@pardus.org.tr>
2024-04-20 12:57:00 +0000

    Update mkiso.sh

--- mkiso.sh
+++ mkiso.sh
@@ -23,20 +23,13 @@ for dir in dev sys proc run tmp ; do
     mount --bind /$dir rootfs/$dir
 done
 # openrc settings
-ln -s openrc-init rootfs/sbin/init || true
-ln -s agetty rootfs/etc/init.d/agetty.tty1 || true
-ln -s agetty rootfs/etc/init.d/agetty.tty2 || true
-ln -s agetty rootfs/etc/init.d/agetty.tty3 || true
-ln -s agetty rootfs/etc/init.d/agetty.tty4 || true
-ln -s agetty rootfs/etc/init.d/agetty.tty5 || true
-ln -s agetty rootfs/etc/init.d/agetty.tty6 || true
 chroot rootfs rc-update add agetty.tty1
-chroot rootfs rc-update add agetty.tty2
-chroot rootfs rc-update add agetty.tty3
-chroot rootfs rc-update add agetty.tty4
-chroot rootfs rc-update add agetty.tty5
-chroot rootfs rc-update add agetty.tty6
-# enable live-config servive
+#chroot rootfs rc-update add agetty.tty2
+#chroot rootfs rc-update add agetty.tty3
+#chroot rootfs rc-update add agetty.tty4
+#chroot rootfs rc-update add agetty.tty5
+#chroot rootfs rc-update add agetty.tty6
+# enable live-config service
 chroot rootfs rc-update add live-config
 # system configuration
 echo -e "live\nlive\n" | chroot rootfs passwd
@@ -72,6 +65,11 @@ if [[ "$FIRMWARE" != "" ]] ; then
     cd ../../..
     rm -rf rootfs/tmp/linux-firmware*
 fi
+# set permissions
+chmod 1777 rootfs/tmp
+chmod 700 rootfs/data/user/root
+chown root:root rootfs/data/user/root
+
 # bind unmount
 for dir in dev sys proc run tmp ; do
     while umount -lf -R rootfs/$dir ; do : ; done
